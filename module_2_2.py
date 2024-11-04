9a6bde3c2d2e79ee1a4941dc084be077aa42bc9f
polskikhdv <polskikhdv@icloud.com>
2024-11-04 15:33:53 +0500

    modified "Одинаковых чисел"

--- module_2_2.py
+++ module_2_2.py
@@ -2,11 +2,11 @@ first = int(input('Введите первое число: '))
 second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
 if first == second and second == third and third == first:
-    print(3)
+    print('Одинаковых чисел:', 3)
 elif first == second or second == third or third == first:
-    print(2)
+    print('Одинаковых чисел:', 2)
 else:
-    print(0)
+    print('Одинаковых чисел:', 0)
 
 
 
