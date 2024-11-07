f356865ca04d306983b4f484ff49c041c2cd6bd8
polskikhdv <polskikhdv@icloud.com>
2024-11-07 22:11:52 +0500

    Вроде правильно

--- module_2_3.py
+++ module_2_3.py
@@ -1,11 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 loop = 0
-while loop < len(my_list):
+while loop < len(my_list) and my_list[loop] > 0:
     print(my_list[loop])
     loop = loop + 1
-    if my_list[loop] > 0:
-        continue
-    else: break
+    if my_list[loop] == 0:
+        loop = loop + 1
+
+
 
 
 
