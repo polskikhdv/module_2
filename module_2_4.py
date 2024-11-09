1082e47b6ff7b333a096f7b3aa0d6fd9e79be53e
polskikhdv <polskikhdv@icloud.com>
2024-11-10 00:19:27 +0500

    Пока что без is_primes

--- module_2_4.py
+++ module_2_4.py
@@ -1,15 +1,12 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-is_prime = True
 for i in numbers[1:]:
     for j in numbers[1:i]:
         if i % j == 0:
-            is_prime = False
             not_primes.append(i)
             break
         else:
-            is_prime = True
             primes.append(i)
             break
 print(primes)
