d574a8fca245fd57fd1f16b540a456702420d500
polskikhdv <polskikhdv@icloud.com>
2024-11-10 01:09:09 +0500

    Чет-нечет, не хватает условий?

--- module_2_4.py
+++ module_2_4.py
@@ -2,12 +2,14 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for i in numbers[1:]:
+    is_prime = True
     for j in numbers[1:i]:
         if i % j == 0:
-            not_primes.append(i)
-            break
+            is_prime = False
         else:
-            primes.append(i)
             break
+    if is_prime == True:
+        primes.append(i)
+    else: not_primes.append(i)
 print(primes)
 print(not_primes)
