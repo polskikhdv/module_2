bd70180f53f66e464c65b63fb11092771955629a
polskikhdv <polskikhdv@icloud.com>
2024-11-10 21:57:14 +0500

    Список списков

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,2 +1,9 @@
 import random
+
 rock_1 = random.choice(range(3, 21))
+result = []
+for a in range(1, rock_1):
+    for b in range(1, rock_1):
+        if rock_1 % (a + b) == 0 and a < b:
+            result.append([a, b])
+print(f'Первый камень выпал: {rock_1}, пароль к выходу: {result}')
