6e0849cf297bfd637e1317d4de520e6bdf2678fa
polskikhdv <polskikhdv@icloud.com>
2024-11-12 22:47:37 +0500

    Исправил усовие проверки для is_prime

--- module_2_4.py
+++ module_2_4.py
@@ -6,29 +6,9 @@ for i in numbers[1:]:
     for j in range(2, i):
         if i % j == 0:
             is_prime = False
-        else:
             break
     if is_prime == True:
         primes.append(i)
     else: not_primes.append(i)
-print(primes)
-print(not_primes)
-
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = []
-not_primes = []
-for number in numbers:
-    if number == 1:
-        continue
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
-    if is_prime:
-        primes.append(number)
-    else:
-        not_primes.append(number)
-print("Простые числа:", primes)
-print("Непростые числа:", not_primes)
+print(f'Простые числа: {primes}')
+print(f'Составные числа: {not_primes}')
